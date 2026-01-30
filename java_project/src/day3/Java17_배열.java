95e28553e2189f1cf9884b188661068c448c41d2
limhyojin <skwlaks1@gmail.com>
2026-01-30 18:31:21 +0900

    2026.01.30 (오후) 배열, 과제

--- java_project/src/day3/Java17_배열.java
+++ java_project/src/day3/Java17_배열.java
@@ -17,17 +17,21 @@ public class Java17_배열 {
 		
 		for(int i = 0; i < arr.length; i++) {
 			
-			arr[i] = ran.nextInt(45) + 1;  //1~45   //arr[0]=39
+			arr[i] = ran.nextInt(45) + 1;  //1~45   //arr[0]=39   //랜덤숫자
 		
 			// i=0 (j(X)), i=1(j=0), i=2(j=0,j=1)
 			
+			
+			// 중복되는게 있는지 비교
+			
 			for(int j = 0; j < i; j++) {
 				// 검증
-				if (arr[i] == arr[j]) {
-					i--;
-					break; //안쪽 반복문(for 문) 빠져나감
+				if (arr[i] == arr[j]) { //i=2 (j=0,j=1) -> arr[2] (arr[0] arr[1])
+					i--;    
+					break;  //중복 찾으면 바로 break;
+							//안쪽 반복문(for 문) 빠져나감 ->바깥쪽 증감식으로간다.
 				}
-			}
+			}//안쪽 반복문(for 문)
 			
 			
 		}
